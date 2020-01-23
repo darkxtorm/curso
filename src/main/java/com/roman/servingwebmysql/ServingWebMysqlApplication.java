be9df4470e15f8eacf8446ca14c9f4554d86ad5b
Jose Raul Roman Mora <raul.roman@outlook.com>
2020-01-23 15:06:04 -0700

    Primer Commit

---
+++ src/main/java/com/roman/servingwebmysql/ServingWebMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.roman.servingwebmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebMysqlApplication.class, args);
+	}
+
+}
